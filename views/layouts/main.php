df557ac361c2cf942874bc783058d32215bad717
Instelce <instelce@protonmail.com>
2024-10-14 14:43:55 +0200

    chore: add .editorconfig, update SETUP.md

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,7 @@
 <?php
+
 use app\core\Application;
+
 ?>
 
 <!doctype html>
@@ -9,55 +11,16 @@ use app\core\Application;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $this->title ?></title>
     <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav" aria-controls="navbarNav"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                           href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
-                    </li>
-
-                    <?php if (!Application::$app->isAuthenticated()): ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/register">Register</a>
-                        </li>
-                    <?php else: ?>
-                        <li>
-                            <a href="/logout" class="nav-link"><?php echo Application::$app->user->firstname; ?> (logout)</a>
-                        </li>
-                        <li>
-                            <a href="/profile" class="nav-link">Profile</a>
-                        </li>
-                    <?php endif; ?>
-                </ul>
-            </div>
+<div>
+    <!-- Show alert -->
+    <?php if (Application::$app->session->getFlash('success')): ?>
+        <div class="alert alert-success">
+            <?php echo Application::$app->session->getFlash('success') ?>
         </div>
-    </nav>
-
-    <div class="container py-4">
-        <?php
-        if (Application::$app->session->getFlash('success')): ?>
-            <div class="alert alert-success">
-                <?php echo Application::$app->session->getFlash('success') ?>
-            </div>
-        <?php endif; ?>
-        {{content}}
-    </div>
+    <?php endif; ?>
+    {{content}}
+</div>
 </body>
 </html>
