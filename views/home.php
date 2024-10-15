75effca9ac7e568ae7d310740d56af53d7f427cc
Instelce <instelce@protonmail.com>
2024-10-15 23:55:51 +0200

    feat: add manage.php script

--- views/home.php
+++ views/home.php
@@ -9,8 +9,9 @@ $this->title = "Home";
 <h1>Home </h1>
 <h2>coucou <?php echo $name ?></h2>
 
-<x-button>coucou</x-button>
-<x-button color="gray">coucou</x-button>
-<x-button color="danger">coucou</x-button>
-<x-button color="purple">coucou</x-button>
-
+<div class="flex gap-2">
+    <x-button>coucou</x-button>
+    <x-button color="gray">coucou</x-button>
+    <x-button color="danger">coucou</x-button>
+    <x-button color="purple">coucou</x-button>
+</div>
