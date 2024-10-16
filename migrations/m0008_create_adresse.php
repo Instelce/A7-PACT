df45c82e469594b10e866a409ec2933dafba340a
valentinConchis <valentin.conchis.pro@gmail.com>
2024-10-16 14:38:31 +0200

    feat: updating migrations files

---
+++ migrations/m0008_create_adresse.php
@@ -0,0 +1,26 @@
+<?php 
+
+use app\core\Application;
+
+class m0008_adresse{
+    public function up(){
+        $db = Application::$app->db;
+        $sql = "CREATE TABLE adresse(
+            id SERIAL PRIMARY KEY,
+            numero INT,
+            nom VARCHAR(255),
+            ville VARCHAR(255),
+            code_postal INT,
+            longitude_user NUMERIC,
+            latitude_user NUMERIC,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );";
+        $db->pdo->exec($sql);
+    }
+
+    public function down(){
+        $sql = "DROP TABLE adresse;";
+        Application::$app->db->pdo->exec($sql);
+    }
+}
--- migrations/m0009_create_table_visite.php
+++ migrations/m0009_create_table_visite.php
@@ -2,21 +2,22 @@
 
 use app\core\Application;
 
-class m0006_taggage{
+class m0009_visite{
     public function up(){
         $db = Application::$app->db;
-        $sql = "CREATE TABLE taggage(
+        $sql = "CREATE TABLE visite(
             id SERIAL PRIMARY KEY,
-            libelle INT,
+            duree NUMERIC,
+            guide BOOLEAN,
+            langue VARCHAR(255),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (offer)
-        );";
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );"; 
         $db->pdo->exec($sql);
     }
 
     public function down(){
-        $sql = "DROP TABLE taggage;";
+        $sql = "DROP TABLE visite;";
         Application::$app->db->pdo->exec($sql);
     }
 }
