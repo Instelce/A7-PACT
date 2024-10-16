09447a53292318a4f470edff047a16f70c750c1e
valentinConchis <valentin.conchis.pro@gmail.com>
2024-10-16 15:32:44 +0200

    feat: updating migrations files

--- migrations/m0006_create_table_offer_tag.php
+++ migrations/m0006_create_table_offer_tag.php
@@ -7,7 +7,10 @@ class m0006_taggage{
         $db = Application::$app->db;
         $sql = "CREATE TABLE taggage(
             id SERIAL PRIMARY KEY,
-            name INT NOT NULL
+            name INT NOT NULL,
+            offer_id INT  NOT NULL,
+            
+            FOREIGN KEY (offer_id) REFERENCES offer(id)
         );";
         $db->pdo->exec($sql);
     }
--- migrations/m0007_create_public_tariff.php
+++ migrations/m0007_create_public_tariff.php
@@ -9,8 +9,9 @@ class m0007_create_public_tariff {
             id SERIAL PRIMARY KEY,
             denomination VARCHAR(255) NOT NULL,
             price NUMERIC NOT NULL
+            offer_id INT NOT NULL
                            
-            FOREIGN KEY (offer) REFERENCES offer(id)  
+            FOREIGN KEY (offer_id) REFERENCES offer(id)  
         );";
         $db->pdo->exec($sql);
     }
