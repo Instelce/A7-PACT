2f53ae711659394a451d94fc5d1a252fadc035ea
stephantoine <124583655+stephantoine@users.noreply.github.com>
2024-10-17 12:11:08 +0200

    chore : update navbar and footer (move to main.php)

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
-import {Footer} from "./components/Footer.js";
 import { Button } from "./components/Button.js";
 import { Input } from "./components/form/Input.js";
 import { Checkbox } from "./components/form/CheckBox.js";
-import { Navbar } from "./components/Navbar.js";
 import { Slider } from "./components/form/Slider.js";
 import {Select} from "./components/form/Select.js";
 
@@ -24,7 +22,5 @@ lucide.createIcons(
 customElements.define("x-checkbox", Checkbox);
 customElements.define("x-input", Input);
 customElements.define("x-button", Button);
-customElements.define("x-navbar", Navbar);
 customElements.define("x-slider", Slider);
 customElements.define("x-select", Select);
-customElements.define('x-footer', Footer);
