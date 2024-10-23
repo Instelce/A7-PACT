3b3d9a03c37f88c770e1de4a9963bd4016af10e1
valentinConchis <valentin.conchis.pro@gmail.com>
2024-10-23 14:30:13 +0200

    feat: professionnal dashboard

--- views/dashboard/offres.php
+++ views/dashboard/offres.php
@@ -15,42 +15,45 @@ var_dump($offersType);
 var_dump(count($offers));*/
 ?>
 
-<body class="font-arial bg-red">
-    <div class="mainContainer">
-        <div class="bloc">
-            <?php foreach ($offers as $i => $offer) { ?>
-                <div class="imgContainer">
-                    <img src="<?php echo $photos[$i]->url_photo ?>" class="w-5vw, h-vw" class="image border-gray-1">
-                </div>
-                <div class="description">
-                    <div class="titleType">
-                        <div class="title">
-                            <h1><strong><?php echo $offer->title ?></strong></h1>
-                        </div>
-                        <span class="offersType"><?php echo $offersType[$i]->type ?></span>
+<div class="mainContainer">
+    <div class="bloc">
+        <?php foreach ($offers as $i => $offer) { ?>
+            <div class="imgContainer">
+                <img src="<?php echo $photos[$i]->url_photo ?>" class="w-5vw, h-vw" class="image border-gray-1">
+            </div>
+            <div class="description">
+                <div class="titleType">
+                    <div class="title">
+                        <h1><strong><?php echo $offer->title ?></strong></h1>
                     </div>
-                    <div>
-                        <p class='mt-3'><?php echo $offer->summary ?></p>
-                    </div>
-                    <p class="location mt-3">Activités • Gratuit</p>
-                    <p class="mt-2">Ploumanach • à 2 min de chez vous • Il y a 2 min</p>
-                    <p class="mt-2">Mise à jour le <?php echo $offer->last_online_date ?></p>
+                    <span class="offersType"><?php echo $offersType[$i]->type ?></span>
                 </div>
-            <?php } ?>
-        </div>
-        <div class="buttons">
-            <button class="button purple mt-3">
-                <i data-lucide="message-square-dot"></i>
-                <?php echo $offer->likes ?>
-            </button>
-            <button class="button gray mt-3">
-                <i data-lucide="message-square-more"></i>
-                <?php echo $offer->likes ?>
-            </button>
-            <button class="button gray mt-3">
-                <i data-lucide="ban"></i>
-                <?php echo $offer->likes ?>
-            </button>
-        </div>
+                <div>
+                    <p class='mt-3'><?php echo $offer->summary ?></p>
+                </div>
+                <p class="location mt-3">Activités • Gratuit</p>
+                <p class="mt-2">Ploumanach • à 2 min de chez vous • Il y a 2 min</p>
+                <p class="borderBottom mt-2">Mise à jour le <?php echo $offer->last_online_date ?></p>
+                <?php if($offer->offer_option) { ?>
+                    <div class="enRelief">
+                        <p><strong>Avec l'option "En relief"</strong></p>
+                    </div>
+                <?php } ?>
+            </div>
+        <?php } ?>
     </div>
-</body>
+    <div class="buttons">
+        <button class="button purple mt-3">
+            <i data-lucide="message-square-dot"></i>
+            <?php echo $offer->likes ?>
+        </button>
+        <button class="button gray mt-3">
+            <i data-lucide="message-square-more"></i>
+            <?php echo $offer->likes ?>
+        </button>
+        <button class="button gray mt-3">
+            <i data-lucide="ban"></i>
+            <?php echo $offer->likes ?>
+        </button>
+    </div>
+</div>
