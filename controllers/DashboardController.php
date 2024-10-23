955ddddbe3782da5637857bc6ea209ddffe04e37
valentinConchis <valentin.conchis.pro@gmail.com>
2024-10-23 12:05:56 +0200

    feat: professionnal dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -7,14 +7,29 @@ use app\core\Controller;
 use app\core\Request;
 use app\core\Response;
 use app\models\offer\Offer;
+use app\models\offer\OfferPhoto;
+use app\models\offer\OfferType;
 
 class DashboardController extends Controller
 {
     public function offers(Request $request, Response $response) {
-        $offers = Offer::find(['id'=>Application::$app->user->account_id]);
+        $this->setLayout('back-office');
+        $offers = Offer::find(['id'=> Application::$app->user->account_id]);
+
+        $photos = [];
+        foreach ($offers as $offer) {
+            $photos[] = OfferPhoto::findOne(['offer_id'=>$offer->id]);
+        }
+
+        $offersType = [];
+        foreach ($offers as $offer) {
+            $offersType[] = OfferType::findOne(['id'=>$offer->offer_type_id]);
+        }
 
         return $this->render('/dashboard/offres', [
             'offers' => $offers ,
+            'photos' => $photos ,
+            'offersType' => $offersType
         ]);
     }
 }
