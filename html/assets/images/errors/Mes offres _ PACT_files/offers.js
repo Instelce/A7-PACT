683891ddbed6752e570ac09e0174dbdfcaff6f90
Instelce <instelce@protonmail.com>
2024-11-25 22:02:01 +0100

    fix: remove files

--- html/assets/images/errors/Mes offres _ PACT_files/offers.js
+++
@@ -1,7 +0,0 @@
-
-let addOptionButton  = document.getElementById('add-option-button');
-let addOptionForm = document.getElementById('add-option-form');
-
-addOptionButton.addEventListener('click', function() {
-    addOptionForm.classList.toggle('hidden');
-});
