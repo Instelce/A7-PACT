76c6935e0c7b1353d80f01e46ff6c9cc90c69114
librossard <librossard@etudiant.univ-rennes1.fr>
2024-11-26 11:03:24 +0100

    fix : navbar, MAJ more filters research

--- views/research.php
+++ views/research.php
@@ -21,57 +21,58 @@ $iconsNames = [
     "ferris-wheel",
 ];
 ?>
-<main>
-    <!-- Search bar, sort and filter -->
-    <div class="flex flex-col mb-4">
-        <x-input rounded>
-            <input slot="input" type="text" placeholder="Rechercher">
-            <button slot="button" class="button only-icon sm">
-                <i data-lucide="search" stroke-width="2"></i>
-            </button>
-        </x-input>
-        <div class="categories-container">
-            <?php
-            foreach ($filtersNames as $key => $filterName) {
-                $iconName = $iconsNames[$key];
-                ?>
-            <button id="<?php echo strtolower($filterName); ?>" class="category-item">
-                <i data-lucide="<?php echo $iconName ?>" class="h-[20px] w-[20px]"></i>
-                <span><?php echo htmlentities($filterName); ?></span>
-            </button>
-            <?php
-            }
+<!-- Search bar, sort and filter -->
+<div class="flex flex-col mb-4">
+    <x-input rounded>
+        <input slot="input" type="text" placeholder="Rechercher">
+        <button slot="button" class="button only-icon sm">
+            <i data-lucide="search" stroke-width="2"></i>
+        </button>
+    </x-input>
+    <div class="categories-container">
+        <?php
+        foreach ($filtersNames as $key => $filterName) {
+            $iconName = $iconsNames[$key];
             ?>
-        </div>
-        <div class="flex gap-2 w-full">
-            <button id="filterButton" class="button gray w-full">
-                <span>Plus de filtres</span>
-                <i data-lucide="sliders-horizontal" class="w-[18px] h-[18px]"> </i>
-            </button>
-            <button class="button gray w-full">
-                <span>A proximité</span>
-                <i data-lucide="navigation" class="w-[18px] h-[18px]"> </i>
-            </button>
-        </div>
+        <button id="<?php echo strtolower($filterName); ?>" class="category-item">
+            <i data-lucide="<?php echo $iconName ?>" class="h-[20px] w-[20px]"></i>
+            <span><?php echo htmlentities($filterName); ?></span>
+        </button>
+        <?php
+        }
+        ?>
     </div>
-    <!-- more filters-->
-    <div id="popup" class="hidden fixed inset-0 bg-black/50 flex justify-center items-center z-50">
-        <div class="popup-content bg-white rounded-lg shadow-lg max-w-[500px] w-full p-6">
-            <!-- Contenu de la popup -->
-            <x-input rounded>
+    <div class="flex gap-2 w-full">
+        <button id="filterButton" class="button gray w-full">
+            <span>Plus de filtres</span>
+            <i data-lucide="sliders-horizontal" class="w-[18px] h-[18px]"> </i>
+        </button>
+        <button class="button gray w-full">
+            <span>A proximité</span>
+            <i data-lucide="navigation" class="w-[18px] h-[18px]"> </i>
+        </button>
+    </div>
+</div>
+<!-- more filters-->
+<div id="popup"
+    class="hidden lg:fixed lg:inset-0 lg:bg-black/50 flex lg:justify-start justify-between items-start z-50">
+    <div
+        class="popup-content bg-white lg:rounded-lg lg:shadow-lg lg:max-w-[400px] w-full h-full mb-1 lg:mb-0 p-2 lg:p-6 lg:pt-[84px]">
+        <!-- Contenu de la popup -->
+        <!-- <x-input rounded>
                 <input slot="input" type="text" placeholder="Rechercher" class="w-full">
                 <button slot="button" class="button only-icon sm">
                     <i data-lucide="search" stroke-width="2"></i>
                 </button>
-            </x-input>
+            </x-input> -->
 
-            <div class="mb-7">
-                <div class="flex flex-col">
-                    <span class="text-base font-bold text-black">Intervalle de prix</span>
-                    <div class="h-px bg-zinc-400 mt-2"></div>
-                </div>
+        <div class="mb-7">
+            <div class="flex flex-col">
+                <span class="text-base font-bold text-black">Intervalle de prix</span>
+                <div class="h-px bg-zinc-400 mt-2"></div>
+            </div>
 
-                <!--
+            <!--
                 <x-select id="category" name="category" required>
                     <label slot="label">Catégorie</label>
                     <span slot="trigger">Choisir une categorie</span>
@@ -85,17 +86,16 @@ $iconsNames = [
                 </x-select>
                 -->
 
-            </div>
-
         </div>
-    </div>
-    </div>
-
-    <!-- Offers, generated in js file -->
-    <div class="flex flex-col gap-2">
-
 
     </div>
-</main>
+</div>
+</div>
+
+<!-- Offers, generated in js file -->
+<div class="flex flex-col gap-2">
+
+
+</div>
 <?php
 ?>
