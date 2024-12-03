a62e09fcc5f1cd919ea1e977f09f1e44c55b81f3
valentinConchis <valentin.conchis.pro@gmail.com>
2024-12-04 00:49:42 +0100

    Modifying the popup on the update-member-account page

--- html/js/pages/registerMember.js
+++ html/js/pages/registerMember.js
@@ -16,3 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateButtonState();
 });
+
+const phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+function formatPhoneInput(event) {
+    const input = event.target;
+    let rawValue = input.value.replace(/\D/g, '');
+    rawValue = rawValue.substring(0, 10);
+    const format = rawValue.replace(/(\d{2})(?=\d)/g, '$1 ').trim();
+    input.value = format;
+}
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', formatPhoneInput);
+});
