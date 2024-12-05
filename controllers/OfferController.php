88b1ca1eab1b3cf96eab8e52ab222f7e5396f6b0
Victouu <rouevictor@gmail.com>
2024-12-05 10:24:19 +0100

    FEAT/ CREATION FABRICE

--- controllers/OfferController.php
+++ controllers/OfferController.php
@@ -248,8 +248,8 @@ class OfferController extends Controller
 
         $openingHours = $offer->schedule();
         $dayOfWeek = strtolower((new DateTime())->format('N'));
-        foreach($openingHours as $openingHour){
-            if($openingHour->day==$dayOfWeek){
+        foreach ($openingHours as $openingHour) {
+            if ($openingHour->day == $dayOfWeek) {
                 $todayHour = $openingHour;
             }
         }
@@ -257,7 +257,7 @@ class OfferController extends Controller
         $closingHour = $todayHour->closing_hours;
         $openingHour = $todayHour->opening_hours;
 
-        if ($todayHour){
+        if ($todayHour) {
             if ($closingHour === 'fermé') {
                 $status = "Fermé";
             } else {
@@ -274,7 +274,9 @@ class OfferController extends Controller
                     $status = "Ouvert";
                 }
             }
-        } else {$status = NULL;}
+        } else {
+            $status = NULL;
+        }
 
 
 
@@ -297,7 +299,7 @@ class OfferController extends Controller
                 $type = "Restaurant";
                 $range_price = RestaurantOffer::findOne(['offer_id' => $id])->range_price;
                 $carte_restaurant = RestaurantOffer::findOne(['offer_id' => $id])->url_image_carte;
-                $price = $range_price === 1 ? "€" : ($range_price === 2 ? "€€" : "€€€");
+                $price = $range_price == 1 ? "€" : ($range_price == 2 ? "€€" : "€€€");
                 $mealsIncluded = "Petit dej";
                 break;
             case 'activity':
