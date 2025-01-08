cdcdf5ad4df1d87c927a15fbd3e58e50a48f9644
Victouu <rouevictor@gmail.com>
2025-01-08 10:04:28 +0100

    feat: protocol.c

---
+++ tchatator/src/protocol.c
@@ -0,0 +1,12 @@
+#include "protocol.h"
+#include "database.h"
+#include "log.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void process_login(){
+    // TODO implement login command
+}
