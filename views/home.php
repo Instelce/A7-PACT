3016fc14497c2a11d23af1a7ca39c0eb36c850c2
stephantoine <124583655+stephantoine@users.noreply.github.com>
2025-01-13 11:05:43 +0100

    chore : update homepage

--- views/home.php
+++ views/home.php
@@ -34,65 +34,67 @@ $this->waves = true;
             </button>
         </x-input>
     </div>
+    <div class="bgcol">
+        <div class="homeDisplayDiv">
+            <h1 class="home-category-title">Destinations phares</h1>
 
-    <div class="homeDisplayDiv">
-        <h1 class="home-category-title">Destinations phares</h1>
+            <div class="carousel-gen" data-slides-visible="3" data-slides-to-scroll="1">
 
-        <div class="carousel-gen" data-slides-visible="3" data-slides-to-scroll="1">
+                <?php foreach ($offersALaUne as $offer) { ?>
+                    <a href="/offres/<?php echo $offer["id"]; ?>">
 
-            <?php foreach ($offersALaUne as $offer) { ?>
-            <a href="/offres/<?php echo $offer["id"]; ?>">
+                        <div class="home-card">
+                            <div class="image">
+                                <img src="<?php echo $offer["image"]; ?>" alt="Image de <?php echo $offer['title']; ?>">
+                            </div>
 
-                <div class="home-card">
-                    <div class="image">
-                        <img src="<?php echo $offer["image"]; ?>" alt="Image de <?php echo $offer['title']; ?>">
-                    </div>
+                            <div class="cardContent">
+                                <h1><?php echo $offer["title"]; ?></h1>
+                                <div class="enLigne">
+                                    <p class="par">Par</p>
+                                    <p class="name"><?php echo $offer["author"]; ?></p>
+                                </div>
 
-                    <div class="cardContent">
-                        <h1><?php echo $offer["title"]; ?></h1>
-                        <div class="enLigne">
-                            <p class="par">Par</p>
-                            <p class="name"><?php echo $offer["author"]; ?></p>
-                        </div>
+                                <div class="enLigneGap">
+                                    <p><?php echo $offer["type"]; ?></p>
+                                    <div class="enLigne">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"
+                                             fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round"
+                                             stroke-linejoin="round" class="lucide lucide-map-pin">
+                                            <path
+                                                d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                                            <circle cx="12" cy="10" r="3" />
+                                        </svg>
+                                        <p><?php echo $offer["location"]; ?></p>
+                                    </div>
+                                </div>
 
-                        <div class="enLigneGap">
-                            <p><?php echo $offer["type"]; ?></p>
-                            <div class="enLigne">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round"
-                                    stroke-linejoin="round" class="lucide lucide-map-pin">
-                                    <path
-                                        d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-                                    <circle cx="12" cy="10" r="3" />
-                                </svg>
-                                <p><?php echo $offer["location"]; ?></p>
+                                <div class="enLigneGap">
+                                    <div class="stars" data-number="<?php echo $offer["rating"] ?>">
+                                    </div>
+                                    <div class="enLigne">
+                                        <p><?php echo $offer["ratingsCount"] ?></p>
+                                        <p>Avis</p>
+                                    </div>
+                                </div>
+
+                                <div class="summary">
+                                    <p><?php echo $offer["summary"]; ?></p>
+                                </div>
+
+                            </div>
+                            <div class="price">
+                                <p class="price-text"><?php echo $offer["price"]; ?></p>
                             </div>
                         </div>
-
-                        <div class="enLigneGap">
-                            <div class="stars" data-number="<?php echo $offer["rating"] ?>">
-                            </div>
-                            <div class="enLigne">
-                                <p><?php echo $offer["ratingsCount"] ?></p>
-                                <p>Avis</p>
-                            </div>
-                        </div>
-
-                        <div class="summary">
-                            <p><?php echo $offer["summary"]; ?></p>
-                        </div>
-
-                    </div>
-                    <div class="price">
-                        <p class="price-text"><?php echo $offer["price"]; ?></p>
-                    </div>
-                </div>
-            </a>
-            <?php } ?>
+                    </a>
+                <?php } ?>
+            </div>
         </div>
     </div>
 
-    <div class="homeDisplayDiv">
+
+    <div class="homeDisplayDiv mb-8">
         <h1 class="home-category-title">Venez découvrir</h1>
 
         <div class="carousel-gen carouselBG" data-slides-visible="4" data-slides-to-scroll="2"
